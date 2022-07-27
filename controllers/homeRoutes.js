aa3d651fb581a0492dde120958a8fac1b70a954a
Lex Matthews <tanukiguernica@gmail.com>
2022-07-27 13:32:09 -0400

    tweaked popular games showcase and game cards

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -13,15 +13,23 @@ router.get('/', async (req, res) => {
     });
 
     const gamesArr=[];
+    const blacklist=[];
 
     for(i=0; i<15;i++){
       let random= Math.floor(Math.random()*popularGamesData.length);
+      if(blacklist.includes(random)){
+        while(blacklist.includes(random)){
+          random= Math.floor(Math.random()*popularGamesData.length);
+        }
+      }
+
       let showcaseItem= popularGamesData[random]
-      
       showcaseGame= showcaseItem.get({plain:true});
+
       gamesArr.push(showcaseGame);
+      blacklist.push(random);
     }
-    
+
     res.render('homepage', { 
       isLogin: false,
       gamesArr,
